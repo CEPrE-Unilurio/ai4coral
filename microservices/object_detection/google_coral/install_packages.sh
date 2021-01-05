41a7d8f13924e2ecf90631c5fd07a15ed856b025
BaniaFonseca <baniafonseca@outlook.com>
2021-01-05 19:08:45 +0200

    pip requirements now includes package version

--- microservices/object_detection/google_coral/install_packages.sh
+++
@@ -1,8 +0,0 @@
-apt-get update
-apt-get install build-essential -y
-apt-get install apt-utils  -y
-
-python -m pip install bottle
-python -m pip install gunicorn
-python -m pip install pip install Pillow
-#python -m pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp38-cp38-linux_x86_64.whl
