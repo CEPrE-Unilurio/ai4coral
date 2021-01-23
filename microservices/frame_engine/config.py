4ce21195d2669af500b4fc7a058499a0d8262827
EmersonAlvaro <hemersonycardoso@gmail.com>
2021-01-23 17:08:34 +0200

    cameraCv refactored

--- microservices/frame_engine/config.py
+++ microservices/frame_engine/config.py
@@ -6,6 +6,8 @@ W = 640
 H = 640
 FPS = 24.0
 
+img_format = '.png'
+
 urlapi = 'http://localhost:8080/detect'
 
 CORAL_DATA_DIR = Path(__file__).parent / "../Coral_Data/"
@@ -13,4 +15,4 @@ CORAL_DATA_DIR = Path(__file__).parent / "../Coral_Data/"
 if not os.path.exists(CORAL_DATA_DIR):
     os.makedirs(CORAL_DATA_DIR)
 
-data_test_directory = Path(__file__).parent / "../object_detection/google_coral/images/"
+data_test_directory = Path(__file__).parent / "../object_detection/images/"
