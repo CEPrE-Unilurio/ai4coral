41ab1137bd607db778ef950f9a618fe9f2ccb072
BaniaFonseca <baniafonseca@outlook.com>
2021-01-30 17:09:18 +0200

    get_output and get_annptation combined

--- microservices/object_detection/annotator.py
+++ microservices/object_detection/annotator.py
@@ -1,9 +1,6 @@
 from settings import common as config
-import detector_base
 from xml.etree import ElementTree as et
 from xml.dom import minidom
-import time
-from timing import timeit
 
 class PascalVocXML:
   """ A Class used to create a pascal VOC XML 
@@ -16,14 +13,12 @@ class PascalVocXML:
 
   def __init__(self, objs=None, filename=None):
     """
+      Init the XML document with basic info
+    
       Args:
         objs: list of detected objects
     """
     self.annotation = et.Element('annotation')
-    self.build_pascal_voc_xml(objs=objs, filename=filename)
-
-  def build_pascal_voc_xml(self, objs=None, filename=None):
-    """ Build a XML document from the list of detected objects """
     folder = et.SubElement(self.annotation, 'folder')
     folder.text = config.FOLDER_NAME
     filename_ = et.SubElement(self.annotation, 'filename')
@@ -37,27 +32,25 @@ class PascalVocXML:
     height.text = str(config.INPUT_SHAPE[0])
     depth = et.SubElement(size, 'depth')
     depth.text = str(config.INPUT_SHAPE[2])
-    LABELS = detector_base.load_labels(config.PATH_TO_LABELS)
     
-    for obj in objs:
-      bbox = obj.bbox
-      object = et.SubElement(self.annotation, 'object')
-      name = et.SubElement(object, "name")
-      name.text = LABELS.get(obj.id, obj.id)
-      bndbox = et.SubElement(object, 'bndbox')
-      xmin = et.SubElement(bndbox, 'xmin')
-      xmin.text = str(bbox.xmin)
-      xmax = et.SubElement(bndbox, 'xmax')
-      xmax.text = str(bbox.xmax)
-      ymin = et.SubElement(bndbox, 'ymin')
-      ymin.text = str(bbox.ymin)
-      ymax = et.SubElement(bndbox, 'ymax')
-      ymax.text = str(bbox.ymax)
-      score = et.SubElement(object, 'score')
-      score.text = str(obj.score)
+  def add_object_element(self, obj=None, label=None):  
+    bbox = obj.bbox
+    object = et.SubElement(self.annotation, 'object')
+    name = et.SubElement(object, "name")
+    name.text = label
+    bndbox = et.SubElement(object, 'bndbox')
+    xmin = et.SubElement(bndbox, 'xmin')
+    xmin.text = str(bbox.xmin)
+    xmax = et.SubElement(bndbox, 'xmax')
+    xmax.text = str(bbox.xmax)
+    ymin = et.SubElement(bndbox, 'ymin')
+    ymin.text = str(bbox.ymin)
+    ymax = et.SubElement(bndbox, 'ymax')
+    ymax.text = str(bbox.ymax)
+    score = et.SubElement(object, 'score')
+    score.text = str(obj.score)
          
-  @timeit
-  def get_annotations(self, **kwargs):
+  def get_annotations(self):
     """ Return a pretty XML string representing the Pascal VOC XML annotation """
     rough_string = et.tostring(self.annotation, 'utf-8')
     reparsed = minidom.parseString(rough_string)
