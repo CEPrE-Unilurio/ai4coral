6bb51b2b8723b740774d0db1ec024349461a24fa
EmersonAlvaro <hemersonycardoso@gmail.com>
2021-02-03 15:21:50 +0200

    cpu monitor add

--- resource/cpu_monitor.py
+++ resource/cpu_monitor.py
@@ -19,10 +19,11 @@ def cpu_stats():
         while True:
             temp = get_temp()
             cpu_percent = psutil.cpu_percent(interval=1)
+            cpu_percents = psutil.cpu_percent(interval=1, percpu=True)
             
             print('Time - {time}  temperature = {temp} - cpu_usage = {cpu} '
              .format(time= time.asctime(), temp= temp, cpu=cpu_percent ))
-
+            print(*('CPU-{} = {}'.format(*k) for k in enumerate(cpu_percents)), sep="   || ")
             sleep(1)
 
     except KeyboardInterrupt:
